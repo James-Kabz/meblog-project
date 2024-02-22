600044d8297ba0b734d9f09704051f29ebb6bb5b
JAMES KABOGO NJOROGE <kabogp@gmail.com>
2024-02-22 22:17:16 +0300

    added a patch https method to be able to edit the body blog and also made it stylish

--- src/components/BlogDetails.js
+++ src/components/BlogDetails.js
@@ -1,34 +1,64 @@
-import { useHistory, useParams } from "react-router-dom/cjs/react-router-dom"
-import UseFetch from "./UseFetch"
-import axios from "axios";
 
+import { useState } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
+import UseFetch from './UseFetch';
+import axios from 'axios';
 
 const BlogDetails = () => {
-    const { id } = useParams()
-    const { data: blogs } = UseFetch(`http://localhost:4002/blogs/` + id);
+    const { id } = useParams();
+    const { data: blogs, error } = UseFetch(`http://localhost:4002/blogs/${id}`);
+    const [newBody, setNewBody] = useState('');
     const history = useHistory();
 
     const handleDelete = (e) => {
         e.preventDefault();
-        axios.delete('http://localhost:4002/blogs/' + id)
+        axios.delete(`http://localhost:4002/blogs/${id}`)
             .then(res => {
-                alert("Blog Deleted Successfully");
+                alert('Blog Deleted Successfully');
                 history.push('/');
-        })
-    }
+                setNewBody('')
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    };
 
+    const handleUpdate = (e) => {
+        e.preventDefault();
+        axios.patch(`http://localhost:4002/blogs/${id}`, { body: newBody })
+            .then(res => {
+                alert('Blog Body Updated Successfully');
+history.push('/');
+                setNewBody('');
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    };
+
+    if (error) return <div>{error}</div>;
     return (
         <div className="details">
             {blogs && (
                 <article>
                     <h3>{blogs.title}</h3>
-                    <p>Done by : {blogs.author}</p>
+                    <p>Done by: {blogs.author}</p>
                     <div>{blogs.body}</div>
-                    <button onClick={handleDelete} variant="danger" className="mt-3" type="submit">Delete</button>
+                    <form onSubmit={handleUpdate}>
+                        <textarea
+                            value={newBody}
+                            onChange={(e) => setNewBody(e.target.value)}
+                            placeholder="Enter new body content..."
+                        ></textarea>
+                        <button type="submit"><i class="fa-solid fa-arrows-rotate"></i></button>
+                    </form>
+                    <button onClick={handleDelete} variant="danger" className="mt-3" type="submit">
+                        <i class="fa fa-trash-o" aria-hidden="true"></i>
+                    </button>
                 </article>
             )}
         </div>
     );
-}
+};
 
 export default BlogDetails;
--- src/components/UseFetch.js
+++ src/components/UseFetch.js
@@ -3,15 +3,21 @@ import axios from "axios";
 
 const UseFetch = (url) => {
     const [data, setData] = useState(null)
+    const [error , setError] = useState(null)
     
     useEffect(() => {
         axios.get(url)
-            .then(res => (setData(res.data)))
-            .catch(err => console.log(err))
+            .then(res => {
+                setData(res.data)
+                setError(null);
+            })
+            .catch(err => {
+                setError(err.res?.data || 'an error occurred');
+            })
         
     }, [url])
     
-    return {data};
+    return {data ,error};
 }
 
 export default UseFetch;
